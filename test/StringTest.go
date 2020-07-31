cd2b0da1ad3230e7e08431d9681b8ddfd74e2c03
tiny.x <185120555@qq.com>
2020-07-31 22:57:20 +0800

    xx
    Signed-off-by: tiny.x <185120555@qq.com>

--- test/StringTest.go
+++ test/StringTest.go
@@ -11,5 +11,7 @@ func main()  {
 	fileInfo, _ := os.Stat("/temp/1.log")
 	formatInt := strconv.FormatInt(int64(fileInfo.Mode().Perm()), 8)
 	fmt.Print(formatInt)
+	fmt.Print(formatInt)
+	fmt.Print(formatInt)
 
 }
