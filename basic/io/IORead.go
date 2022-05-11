58a641ad864d6d72c38f6383a9aab0efdc8a0948
tiny-x <xf.yefei@gmail.com>
2022-05-11 10:24:44 +0800

    测试

--- basic/io/IORead.go
+++ basic/io/IORead.go
@@ -2,13 +2,38 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"github.com/ncw/directio"
+	"io"
+	"os"
 )
 
 func main() {
-	file, err := ioutil.ReadFile("/dev/zero")
+	var block = 1024 * 1024
+	blocks := make([]byte, block)
+	f, err := directio.OpenFile("/tmp/1.log", os.O_CREATE|os.O_WRONLY|os.O_RDONLY, 0666)
+	if err != nil {
+		fmt.Print(err)
+	}
+	for i := 0; i < 1024; i++ {
+		f.Write(blocks)
+	}
+	f.Close()
+
+	f, err = directio.OpenFile("/tmp/1.log", os.O_RDONLY, 0666)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(file))
+	for {
+		_, err := io.ReadFull(f, blocks)
+		if err != nil {
+			if err == io.EOF {
+				f, err = directio.OpenFile("/tmp/1.log", os.O_RDONLY, 0666)
+				if err != nil {
+					fmt.Println(err)
+				}
+			} else {
+				fmt.Println(err)
+			}
+		}
+	}
 }
--- basic/io/IOWrite.go
+++ basic/io/IOWrite.go
@@ -10,16 +10,21 @@ import (
 var block = 1024 * 1024
 
 func main() {
-	blocks := make([]byte, block)
+
 	for {
-		f, err := directio.OpenFile("/tmp/1.log", os.O_CREATE|os.O_WRONLY, 0666)
-		if err != nil {
-			fmt.Print(err)
-		}
-		for i := 0; i < 1024; i++ {
-			f.Write(blocks)
-		}
-		f.Close()
+		createFile()
 		os.Remove("/tmp/1.log")
 	}
+}
+
+func createFile() {
+	blocks := make([]byte, block)
+	f, err := directio.OpenFile("/tmp/1.log", os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Print(err)
+	}
+	for i := 0; i < 1024; i++ {
+		f.Write(blocks)
+	}
+	f.Close()
 }
